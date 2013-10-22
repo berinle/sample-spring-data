5ff3e5d13cc84ed5dfee05fe3ea8a69f5abf0739
Bayo Erinle <berinle@gmail.com>
2013-10-22 17:20:44 -0400

    added converters

--- src/main/java/com/myapp/Course.java
+++ src/main/java/com/myapp/Course.java
@@ -1,6 +1,9 @@
 package com.myapp;
 
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * @author berinle
@@ -24,5 +27,29 @@ public class Course {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Component
+    static class CourseToStringConverter implements Converter<Course, String> {
+
+        @Override
+        public String convert(Course source) {
+            return source == null? null: source.id;
+        }
+    }
+
+    @Component
+    static class StringToCourseConverter implements Converter<String, Course> {
+
+        @Override
+        public Course convert(String source) {
+            if(StringUtils.hasText(source)){
+                Course course = new Course();
+                course.setId(source);
+                return course;
+            } else {
+                return null;
+            }
+        }
     }
 }
